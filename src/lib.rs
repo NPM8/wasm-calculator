97c9b88998790ef3d531ef7bb454f0c89f64b50a
npm8 <nnnp.dawid@gmail.com>
2019-08-29 11:11:37 +0200

    Few changes

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,8 @@ use wasm_bindgen::prelude::*;
 
 mod calc;
 
-// Called by our JS entry point to run the example
 #[wasm_bindgen]
 pub fn doMath(val1: i32, val2: i32, sign: String) -> Result<i32, JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // // window object.
-    // let window = web_sys::window().expect("no global `window` exists");
-    // let document = window.document().expect("should have a document on window");
-    // let body = document.body().expect("document should have a body");
-
-    // // Manufacture the element we're gonna append
-    // let val = document.create_element("p")?;
-    // val.set_inner_html("test");
-
-    // body.append_child(&val)?;
 
     let calculator = calc::ToCal::new(val1, val2);
 
